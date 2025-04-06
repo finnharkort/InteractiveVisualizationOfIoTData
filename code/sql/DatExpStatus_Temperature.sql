ae9a2e70fe7d840d963be5d50624c9c81c75f215
Finn Harkort-Berge <finnhb@kth.se>
2025-04-06 15:41:28 +0200

    update code

--- code/sql/DatExpStatus_Temperature.sql
+++ code/sql/DatExpStatus_Temperature.sql
@@ -11,13 +11,13 @@ SELECT DISTINCT TOP 90000 t.Timestamp,
                     m.Name AS MaterialName, -- Added this line to get the material's name
  e.CodeName AS EquipmentName -- Changed the alias to EquipmentName
  
-FROM DatExpStatus_Temperature t 
-LEFT JOIN DatExpComponent c ON t.Component = c.ID 
-LEFT JOIN DatExpOrder o ON t.[Order] = o.ID 
+FROM DatExpStatus_Temperature t
+LEFT JOIN DatExpComponent c ON t.Component = c.ID
+LEFT JOIN DatExpOrder o ON t.[Order] = o.ID
 LEFT JOIN DatExpMaterial m ON o.Material = m.ID -- New join for DatExpMaterial
  
 LEFT JOIN DatExpEquipment e ON c.Equipment = e.ID -- New join for DatExpEquipment
  
-WHERE t.Timestamp >= DATEADD(DAY, -7, GETUTCDATE()) 
+WHERE t.Timestamp >= DATEADD(DAY, -7, GETUTCDATE())
     AND t.[Order] <> '00000000-0000-0000-0000-000000000000'
 ORDER BY t.Timestamp DESC
